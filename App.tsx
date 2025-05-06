c18123bc31f24557590e3f9186cd2f097eaed08f
GhoRid <pgh5489@gmail.com>
2025-05-06 18:17:08 +0900

    feat: react-query 설치

--- App.tsx
+++ App.tsx
@@ -1,15 +1,26 @@
 import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
-import {Text, View} from 'react-native';
 import StackNavigator from './src/navigators/StackNavigator';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    // queries: {
+    //   refetchOnWindowFocus: false,
+    //   retry: false,
+    // },
+  },
+});
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <NavigationContainer>
-        <StackNavigator />
-      </NavigationContainer>
+      <QueryClientProvider client={queryClient}>
+        <NavigationContainer>
+          <StackNavigator />
+        </NavigationContainer>
+      </QueryClientProvider>
     </SafeAreaProvider>
   );
 }
