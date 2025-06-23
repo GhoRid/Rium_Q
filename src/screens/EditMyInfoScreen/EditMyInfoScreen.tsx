ad7e42746756c02011b85c8d18e2b38d61500c91
GhoRid <pgh5489@gmail.com>
2025-06-23 11:32:22 +0900

    feat: 내 정보 수정 화면 완성

--- src/screens/EditMyInfoScreen/EditMyInfoScreen.tsx
+++ src/screens/EditMyInfoScreen/EditMyInfoScreen.tsx
@@ -1,18 +1,21 @@
 import {NavigationProp, useNavigation} from '@react-navigation/native';
-import {View, Text, Pressable, StyleSheet} from 'react-native';
+import {View, StyleSheet} from 'react-native';
 import {RootStackParamList} from '../../types/screens';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import CustomHeader from '../../components/Header/CustomHeader';
 import BackButtonHeaderLeft from '../../components/Header/BackButtonHeaderLeft';
 import EditProfileImage from './components/EditProfileImage';
+import EditList from './components/EditList';
 
 const EditMyInfoScreen = () => {
   const navigation = useNavigation<NavigationProp<RootStackParamList>>();
   return (
     <SafeAreaView style={styles.contianer}>
       <CustomHeader leftItem={<BackButtonHeaderLeft screenName="내 정보" />} />
-      <View>
+      <View style={styles.contentContainer}>
         <EditProfileImage />
+
+        <EditList />
       </View>
     </SafeAreaView>
   );
@@ -23,5 +26,8 @@ const styles = StyleSheet.create({
   contianer: {
     flex: 1,
     backgroundColor: '#fff',
+  },
+  contentContainer: {
+    padding: 20,
   },
 });
---
+++ src/screens/EditMyInfoScreen/components/EditList.tsx
@@ -0,0 +1,56 @@
+import {StyleSheet, View, Text} from 'react-native';
+import SvgIcon from '../../../components/SvgIcon';
+
+const DATA = [
+  {
+    label: '거주 지역',
+    value: '광주광역시 북구 지산동',
+  },
+  {
+    label: '(출신) 학교',
+    value: '조선대학교여자고등학교',
+  },
+  {
+    label: '학년',
+    value: '2학년',
+  },
+];
+
+const EditList = () => {
+  return (
+    <>
+      {/* 정보 항목 */}
+      {DATA.map((item, index) => (
+        <View style={styles.infoItem} key={index}>
+          <View style={styles.vertacal}>
+            <Text style={styles.label}>{item.label}</Text>
+            <Text style={styles.value}>{item.value}</Text>
+          </View>
+          <SvgIcon name="우측방향" size={24} color="#BDBDBD" />
+        </View>
+      ))}
+    </>
+  );
+};
+
+export default EditList;
+
+const styles = StyleSheet.create({
+  infoItem: {
+    marginBottom: 24,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  label: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  vertacal: {
+    gap: 10,
+  },
+  value: {
+    fontSize: 18,
+    color: '#333',
+  },
+});
