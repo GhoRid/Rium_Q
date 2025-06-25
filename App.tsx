edcb379f8279942179bac7a947e81ce218b6fb23
GhoRid <pgh5489@gmail.com>
2025-06-25 16:24:12 +0900

    feat: NOTO sans설치

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import SplashScreen from './src/screens/SplashScreen';
 import StackNavigator from './src/navigators/StackNavigator';
-import {checkAuth} from './src/services/auth/checkAuth'; // ✅ 서비스 로직 임포트
+import {checkAuth} from './src/services/auth/checkAuth';
 import LoginScreen from './src/screens/LoginScreen/LoginScreen';
 
 const queryClient = new QueryClient();
--- src/styles/palette.ts
+++ src/styles/palette.ts
@@ -2,10 +2,6 @@ const palette = {
   app_main_color: '#1C2E4A',
   app_blue: '#0667FF',
   kakao_yellow: '#FEE500',
-  //   red_heavy: '#B55D66',
-  white: '#FFFFFF',
-  gray: '#8B8989',
-  black: '#0F0B03',
 };
 
 export default palette;
