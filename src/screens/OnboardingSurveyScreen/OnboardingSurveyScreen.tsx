d80c251def437a40c7c6b3a00496dccea0a32495
GhoRid <pgh5489@gmail.com>
2025-06-30 17:21:10 +0900

    feat: 설문 페이지 하단 버튼 완성

--- src/screens/OnboardingSurveyScreen/OnboardingSurveyScreen.tsx
+++ src/screens/OnboardingSurveyScreen/OnboardingSurveyScreen.tsx
@@ -1,17 +1,44 @@
-import {ScrollView, StyleSheet, View} from 'react-native';
-import AppText from '../../components/AppText';
+import {Dimensions, ScrollView, StyleSheet, View} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import CustomHeader from '../../components/Header/CustomHeader';
 import BackButtonHeaderLeft from '../../components/Header/BackButtonHeaderLeft';
 import SurveyProgressBar from './components/SurveyProgressBar';
+import StepRegion from './steps/StepRegion';
+import {useRef, useState} from 'react';
+import BottomButton from './components/BottomButton';
+
+const {width} = Dimensions.get('window');
 
 const OnboardingSurveyScreen = () => {
+  const scrollRef = useRef<ScrollView>(null);
+  const [currentStep, setCurrentStep] = useState(0);
+
+  const goToStep = (step: number) => {
+    setCurrentStep(step);
+    scrollRef.current?.scrollTo({x: width * step, animated: true});
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <CustomHeader leftItem={<BackButtonHeaderLeft />} />
-      {/* 프로그레스 바 */}
-      <SurveyProgressBar currentStep={0} totalSteps={5} />
-      <ScrollView pagingEnabled={true}>{/* 스텝별 페이지 내용 */}</ScrollView>
+      <SurveyProgressBar currentStep={currentStep} totalSteps={2} />
+
+      <ScrollView
+        ref={scrollRef}
+        scrollEnabled={false} // 터치 스크롤 비활성화
+        pagingEnabled
+        horizontal
+        showsHorizontalScrollIndicator={false}>
+        <View style={styles.stepContainer}>
+          <StepRegion />
+        </View>
+        <View style={styles.stepContainer}>
+          <StepRegion />
+        </View>
+      </ScrollView>
+
+      {/* 하단 버튼 */}
+      <BottomButton currentStep={currentStep} goToStep={goToStep} />
     </SafeAreaView>
   );
 };
@@ -22,5 +49,21 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+  },
+  buttonRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 24,
+  },
+  button: {
+    paddingVertical: 12,
+    paddingHorizontal: 20,
+    borderRadius: 8,
+    backgroundColor: '#eee',
+  },
+  stepContainer: {
+    width: width,
+    paddingHorizontal: 24,
+    paddingTop: 16,
   },
 });
--- src/screens/OnboardingSurveyScreen/steps/StepRegion.tsx
+++ src/screens/OnboardingSurveyScreen/steps/StepRegion.tsx
@@ -1,9 +1,14 @@
 import {Dimensions, View} from 'react-native';
+import AppText from '../../../components/AppText';
 
 const {width} = Dimensions.get('window');
 
 const StepRegion = () => {
-  return <View></View>;
+  return (
+    <View>
+      <AppText>현재 거주하고 있는 지역</AppText>
+    </View>
+  );
 };
 
 export default StepRegion;
